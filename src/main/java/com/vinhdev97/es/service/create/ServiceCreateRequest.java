dda9f01321b6be395a998e743815e5832bdccb10
vinhdev97 <vinh@Vinhs-MacBook-Pro.local>
2023-06-10 20:03:02 +0700

    week 4

---
+++ src/main/java/com/vinhdev97/es/service/create/ServiceCreateRequest.java
@@ -0,0 +1,28 @@
+package com.vinhdev97.es.service.create;
+
+import com.vinhdev97.es.entity.Service;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@Builder
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class ServiceCreateRequest {
+
+  @NotNull(message = "Tên là trường bắt buộc.")
+  @Size(max = 256, message = "Tên tối đa 256 ký tự.")
+  private String name;
+
+  public Service.ServiceBuilder toServiceBuilder() {
+    return Service.builder().name(this.name);
+  }
+}
--- src/main/java/com/vinhdev97/es/service/search/ServiceSearchController.java
+++ src/main/java/com/vinhdev97/es/service/search/ServiceSearchController.java
@@ -4,7 +4,6 @@ import com.vinhdev97.es.entity.Service;
 import com.vinhdev97.es.repository.ServiceRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
